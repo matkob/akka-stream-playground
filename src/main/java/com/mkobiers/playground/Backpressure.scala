e6890dc16a61389ca1ed97e634776039f1ce3896
mkobierski <mkobierski@match-trade.com>
2020-07-23 09:05:00 +0200

    buffer tests

--- src/main/java/com/mkobiers/playground/Backpressure.scala
+++ src/main/java/com/mkobiers/playground/Backpressure.scala
@@ -3,7 +3,7 @@ package com.mkobiers.playground
 import akka.Done
 import akka.actor.ActorSystem
 import akka.stream.OverflowStrategy
-import akka.stream.scaladsl.{Keep, RunnableGraph, Sink, Source}
+import akka.stream.scaladsl.{Flow, Keep, RunnableGraph, Sink, Source}
 
 import scala.concurrent.Future
 
@@ -12,12 +12,16 @@ object Backpressure {
   implicit val system: ActorSystem = ActorSystem("playground")
 
   def main(args: Array[String]): Unit = {
-    val source = Source(List(1,2,3,4,5,6,7,8,9)).buffer(1, OverflowStrategy.dropHead)
+    val source = Source(List(1,2,3,4,5,6,7,8,9)).buffer(2, OverflowStrategy.dropTail)
+    val flow = Flow.fromFunction[Int, Int](n => {
+      println(s"$n passing by")
+      n
+    })
     val sink = Sink.foreach[Int](n => {
       println(f"received $n")
       Thread.sleep(1000)
     })
-    source.async.toMat(sink)(Keep.left).run()
+    source.async.via(flow).async.to(sink).run()
   }
 
 }
